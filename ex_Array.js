a758544a196accbea64795299b47688c640e5f86
EliasInfnet <elias@uakti.com>
2021-12-06 20:31:00 -0300

    terceiro commit

--- ex_Array.js
+++ ex_Array.js
@@ -1,8 +1,9 @@
 const numbers = [2, 8, 16];
 const users = [{ id: 4781, name: 'Juan Carlos' }, { id: 6538, name: 'Maria Benita' }, { id: 3874, name: 'Manuela Assis' }]
 
+
+/* 1 - Mapas */
 const mostrarMapas = () => {
-  /* 1 - Mapas */
   /* 1) Faça um map no array 'numbers', para que cada valor do array, seja multiplicado por 3 */
   console.log(numbers.map(num => num * 3))
   /* 2) Faça um map no array 'numbers', para que cada valor do array, seja somado com o último elemento */
@@ -13,8 +14,9 @@ const mostrarMapas = () => {
 
 /* mostrarMapas() */
 
+
+/* 2 - Filtros */
 const mostrarFiltros = () => {
-  /* 2 - Filtros */
   /* 1) Faça um filter no array 'numbers', para que não contenha valores menores que 9 */
   console.log(numbers.filter(n => n >= 9))
   /* 2) Faça um filter no array 'numbers', para que não contenha dados do tipo número */
@@ -25,13 +27,34 @@ const mostrarFiltros = () => {
 
 // mostrarFiltros()
 
+
 /* 3 - Reduções */
-/* 1) Faça um reduce no array 'users', para que cada objeto não contenha a propriedade 'name' */
-/* 2) Faça um reduce no array 'users', retornando um objeto com a seguinte estrutura: { [name]: { id: 4781 } } */
+const mostrarReduce = () => {
+
+  /* 1) Faça um reduce no array 'users', para que cada objeto não contenha a propriedade 'name' */
+  const _users = users.reduce((accumulator, currentValue) => {
+    return [...accumulator, { id: currentValue.id }]
+  }, [])
+
+  console.log(_users)
+
+  /* 2) Faça um reduce no array 'users', retornando um objeto com a seguinte estrutura: { [name]: { id: 4781 } } */
+  const _users2 = users.reduce((accumulator, currentValue) => {
+    return {
+      ...accumulator,
+      [currentValue.name]: { id: currentValue.id }
+    }
+  }, [])
+
+  console.log(_users2)
+
+}
+
+mostrarReduce()
 
 
+/* 4 - Busca */
 const mostrarBusca = () => {
-  /* 4 - Busca */
   /* 1) Faça um find no array 'users', buscando o objeto com a propriedade name igual a 'Manuela Assis' */
   console.log(users.find(u => u.name == 'Manuela Assis'))
   /* 2) Faça um find no array 'numbers', buscando o valor menor que 15 */
@@ -46,9 +69,8 @@ const mostrarBusca = () => {
 
 // mostrarBusca()
 
+/* 5 - Desestruturação */
 const mostrarDesestruturacao = () => {
-  
-  /* 5 - Desestruturação */
   /* 1) Faça uma desestruturação do array 'users', extraindo o primeiro e segundo valor */
   const [firstUser, secondUser] = users
   console.log(firstUser)
@@ -59,27 +81,46 @@ const mostrarDesestruturacao = () => {
   const { name } = user
   console.log(name)
 
-  /* 3) Faça uma desestruturação do array 'users', extraindo a propriedade id do primeiro valor, e name do segundo valor */  
-  const [{id: idPrimeiro},  {name: nameSegundo}] = users
+  /* 3) Faça uma desestruturação do array 'users', extraindo a propriedade id do primeiro valor, e name do segundo valor */
+  const [{ id: idPrimeiro }, { name: nameSegundo }] = users
   console.log(idPrimeiro)
   console.log(nameSegundo)
 
   // const [primeiroUsuario , segundoUsuario] = users
   // console.log(primeiroUsuario.id)
   // console.log(segundoUsuario.name)
-
 }
 
-mostrarDesestruturacao()
+// mostrarDesestruturacao()
 
+
+/* 6 - Spread Operator */
 const mostrarSpreadOperator = () => {
-  /* 6 - Spread Operator */
   /* 1) Crie um novo array com os valores dos arrays 'number' e 'users', usando o spread operator */
   console.log([...numbers, ...users])
 }
 
+
 /* 7 - Inserção */
-/* 1) Faça um push no array 'numbers', adicionado o valor 45 */
-/* 2) Faça um push no array 'users', adicionando o valor {id: 8893, name: 'Dominique Carlos'} */
-/* 3) Faça um unshift no array 'users', adicionando o valor {id: 7436, name: 'Mariano Azevedo'} */
-/* 4) Faça um unshift no array 'numbers', adicionando os valores -20, -7, 89 */
+const mostrarInsercao = () => {
+
+  /* 1) Faça um push no array 'numbers', adicionado o valor 45 */
+  numbers.push(45)
+  console.log(numbers)
+
+  /* 2) Faça um push no array 'users', adicionando o valor {id: 8893, name: 'Dominique Carlos'} */
+  users.push({ id: 8893, name: 'Dominique Carlos' })
+  console.log(users)
+
+  /* 3) Faça um unshift no array 'users', adicionando o valor {id: 7436, name: 'Mariano Azevedo'} */
+  users.unshift({ id: 7436, name: 'Mariano Azevedo' })
+  console.log(users)
+
+  /* 4) Faça um unshift no array 'numbers', adicionando os valores -20, -7, 89 */
+  const numerosParaAdicionar = [-20, -7, 89]
+  numbers.unshift(...numerosParaAdicionar)
+  console.log(numbers)
+
+}
+
+// mostrarInsercao()
--- exemplo.js
+++ exemplo.js
@@ -3,9 +3,12 @@
 // const { id, nome } = carrinho
 // console.log(nome)
 
+const numbers = [2, 8, 16];
+const users = [{ id: 4781, name: 'Juan Carlos' }, { id: 6538, name: 'Maria Benita' }, { id: 3874, name: 'Manuela Assis' }]
+
 const listedString = "Banana, apple, pear, orange";
 
-const {location} = {
+const { location } = {
   location: {
     country: 'Brasil',
     state: 'Rio de Janeiro',
@@ -16,14 +19,40 @@ const {location} = {
   }
 };
 
-const { country, state, address} = location
-const { street, number} = address
+const { country, state, address } = location
+const { street, number } = address
 
 // console.log(location)
 // console.log(address)
 
-const teste = [undefined, null, '','teste']
+const teste = [undefined, null, '', 'teste']
 
-teste.forEach(t =>{
+teste.forEach(t => {
   console.log(!!t)
 })
+
+const _users = users.reduce((accumulator, currentValue) => {
+  return{
+    names: [...accumulator.names, currentValue.name],
+    ids: [...accumulator.ids, currentValue.id]
+  }
+}, {
+  names: [],
+  ids: []
+})
+
+const selectedPayments = [{type: 'food-card'}];
+
+const selectedPaymentsTest = selectedPayments.reduce((accumalator, currentValue) => {	
+	return {
+		types: [...accumalator.types, currentValue.type],
+	}
+}, {
+	types: ['credit-card', 'food-card'],
+});
+
+const splitTypesGroup = ['credit-card', 'food-card'];
+
+const isPaymentValid = selectedPaymentsTest.types.every(selectedType => splitTypesGroup.some(typeInGroup => typeInGroup === selectedType));
+
+console.log(isPaymentValid);
